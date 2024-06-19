627b745fe7f016e6270abaeed4652bf96426aec0
aid219 <aid219@mail.ru>
2024-06-20 00:03:43 +0300

    add test mail sender

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rabskieOpovesheniya
 when rabbits call u , will u answer them? Answer for that question u need to find by yourself, just follow by your heart....
+
+ltpiedglwsdvbomb
--- main.go
+++ main.go
@@ -1,8 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"gopkg.in/mail.v2"
+)
 
 func main() {
-	fmt.Println("ebanii github")
-	fmt.Println("sosissssska")
+	// Создаем новое сообщение
+	m := mail.NewMessage()
+
+	// Устанавливаем отправителя
+	m.SetHeader("From", "qum.my@yandex.ru")
+	// reader := bufio.NewReader(os.Stdin)
+	// fmt.Print("Enter text: ")
+	// text, _ := reader.ReadString('\n')
+	// fmt.Println(text)
+	// Устанавливаем получателя
+	m.SetHeader("To", "ahn.ngx@gmail.com")
+
+	// Устанавливаем тему письма
+	m.SetHeader("Subject", "Письмо плюшке")
+
+	// Устанавливаем тело письма
+	m.SetBody("text/plain", "love love love")
+
+	// Настройка SMTP сервера
+	d := mail.NewDialer("smtp.yandex.ru", 587, "qum.my@yandex.ru", "ltpiedglwsdvbomb")
+	// m.SetHeader("MIME-Version", "1.0")
+	// m.SetHeader("Content-Type", "text/plain; charset=\"UTF-8\"")
+	// sender(d, m)
+	// Отправка сообщения
+	if err := d.DialAndSend(m); err != nil {
+		log.Fatal(err)
+	}
 }
+
+// func sender(dd *mail.Dialer, mm *mail.Message) {
+// 	i := " love"
+
+// 	for {
+// 		i += i
+// 		mm.SetBody("text/plain", "love love love"+i)
+// 		if err := dd.DialAndSend(mm); err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		time.Sleep(5 * time.Second)
+// 	}
+
+// }
