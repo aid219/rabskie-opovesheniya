35a2335e4df3e1f381d5fede265315ca54496d5b
aid219 <aid219@mail.ru>
2024-07-01 15:41:12 +0300

    sdf

--- main.go
+++ main.go
@@ -15,8 +15,11 @@ func main() {
 	if err != nil {
 		log.Error("Error setup logger", err)
 	}
+	config.ConfigData, err = config.LoadConfig(log, "local2.txt", []byte("qummy1234567890"))
 
-	config.ConfigData = config.LoadConfig(log)
+	if err != nil {
+		log.Error("Error load config", err)
+	}
 
 	ch, que, err := rabbit.Init(log, config.ConfigData.Rabbit.Host, config.ConfigData.Rabbit.Queue)
 
